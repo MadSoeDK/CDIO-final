8a7499512e9559c81af6b109eb0a12ebb1869b6d
mamm1504 <s215805@student.dtu.dk>
2022-01-04 09:29:05 +0100

    GUI Player is now called guiPlayer

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -6,11 +6,11 @@ import java.awt.Color;
 public class Player {
     private int balance, placement;
     private String name;
-    private GUI_Player player;
+    private GUI_Player guiPlayer;
 
     public Player(String name, int balance, Color color) {
         this.balance = balance;
-        this.player = new GUI_Player(name, balance, new GUI_Car(color,color, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL));
+        this.guiPlayer = new GUI_Player(name, balance, new GUI_Car(color,color, GUI_Car.Type.CAR, GUI_Car.Pattern.FILL));
         this.name = name;
     }
 
@@ -19,19 +19,19 @@ public class Player {
     }
     public void setBalance(int amount) {balance = amount;}
     public GUI_Player getPlayer(){
-        return this.player;
+        return this.guiPlayer;
     }
     public int getPlacement() {
         return placement;
     }
-    public void gotoPlacement(int placement) { this.placement = placement; }
     public void setPlacement(int sum) {
         placement += sum;
     }
+    public void gotoPlacement(int placement) { this.placement = placement; }
     public String getName() {
         return name;
     }
     public Color getPlayerColor() {
-        return player.getPrimaryColor();
+        return guiPlayer.getPrimaryColor();
     }
 }
